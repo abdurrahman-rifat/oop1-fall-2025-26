a6e0effba174018f71a7a649d0824f8c18beef94
abdurrahman-rifat <abdurrahmanrifat122@gmail.com>
2025-10-28 17:07:52 +0600

    25-62927-2

---
+++ Avg.java
@@ -0,0 +1,9 @@
+public class Avg {
+   public static void main(String[] args) {
+    int mun1=4;
+    int mun2=5;
+    int mun3=8;
+    float avg=(mun1+mun2+mun3)/3;
+    System.out.println( "Average is:"+avg);
+   } 
+}
