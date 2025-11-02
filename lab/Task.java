0e74400e57112a2900708420fa26576b88689d6f
abdurrahman-rifat <abdurrahmanrifat122@gmail.com>
2025-11-02 17:00:50 +0600

    25-62927-2

---
+++ lab/Task.java
@@ -0,0 +1,57 @@
+public class Task{
+    public static void  main(String args[]){
+        //name
+        String fast_name="Tonoy";
+        String last_name="Saha";
+        System.out.println(fast_name+" " +last_name);
+
+      //productcode
+        String name="DUO";
+    
+        int id=9009;
+        String code =name+" "+id;
+        System.out.println("Generated product code:"+code);
+      
+        //emailname
+        
+        String email = "rifat122@gmail.com";
+        String extract = email.substring(0,9);
+        System.out.println("Email:"+email);
+
+
+         //sentence
+         String sentence = "I am a disco dancer";
+         int totalwords = sentence.length();
+         System.out.println("Total number of Words :"+totalwords);
+    
+    
+
+
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
